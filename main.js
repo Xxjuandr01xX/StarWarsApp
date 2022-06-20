41fff3064394949ad824d78358fb20e2e9145143
Juan D Rincon U <jd.rincon021@gmail.com>
2022-06-19 23:20:54 -0400

    paginacion, e inicio de vista de detalle

--- main.js
+++ main.js
@@ -9,13 +9,50 @@ let character_search = () =>{
 	}
 }
 
+let display_modal = (modal_id, switsh) => {
+	/**
+	 * Funcion para mostrar una ventana modal*/
+	 let modal = new bootstrap.Modal(document.getElementById(modal_id), {
+	 	'backdrop' : true
+	 });
+
+	 modal.show();
+}
+
+let lista_personajes = (numero) =>{
+	let nombres = "";
+	fetch('https://swapi.dev/api/people/?page='+numero).
+	then(response => response.json()).
+	then(data => {
+		data.results.forEach((characters) => {
+			const people = characters.url.split("/");			
+			nombres += "<li class = 'nav-item border border-buttom mb-2'><a class = 'btn btn-outline-warning w-100' href = 'detail.html?people="+people[5]+"'>"+characters.name+"</a></li>";
+		});
+		document.getElementById('cargar_nombres').innerHTML = nombres;
+	});
+}
 
 
-let next_page = (int) => {
- /**
-  *  Funcion para pasar a la siguiente lista de personajes.
-  * */
+let paginacion = ()=>{
+	let paginas = "";
+	let nro_paginas = document.getElementsByName('page[]');
+	for(let i = 1; i < 10; i++){
+		/**
+		 *  Aqui se muestra la paginacion.
+		 **/
+		paginas += "<li class='page-item'><a class='page-link' href='#' name = 'page[]'>"+i+"</a></li>";
+	}
+	console.log(nro_paginas);
+	document.getElementById('paginacion').innerHTML = paginas;
+	for(let j=0; j < 10; j++){
+		nro_paginas[j].addEventListener('click', (event)=>{
+			event.preventDefault();
+			lista_personajes(j+1);
+		});
+	}
 }
 
 //character_search();
-lista_personajes();
+
+lista_personajes(1);
+paginacion();
